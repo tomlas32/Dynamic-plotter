4b2cc46040091b233bf0f6a529e24dfffb438ef4
Tomasz Lasota <tomlas32@gmail.com>
2024-04-28 15:47:46 +0100

    Add feature: Implement serial port monitor class for fetching and updating available com ports

--- dynamic_serial_plotter.py
+++ dynamic_serial_plotter.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import pyqtgraph as pgt
+from PyQt5.QtCore import QTimer, QObject, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QFileDialog, QMainWindow, QWidget, QPushButton, QComboBox, QMessageBox
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 import csv
@@ -27,7 +28,32 @@ class CircularBuffer:
         self.buffer[self.index] = value                                                             # assigns pushed value to the current index element in the numpy list
         self.index = (self.index + 1) % self.max_size                                               # increment index and checks if buffer was filled 
         if self.index == 0:
-            self.full = True   
+            self.full = True  
+
+
+# class related to monitoring Serial Port activity 
+class SerialPortMonitor(QObject):
+    port_changed = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.check_ports)
+        self.timer.start(1000)                                                                      # checks current ports every 3 seconds
+
+        self.current_ports = []                                                                     # list for storing currently available ports
+    
+    # function for checking currently available ports
+    def check_ports(self):
+        available_ports = self.get_ports()
+        if available_ports != self.current_ports:
+            self.current_ports = available_ports
+            self.port_changed.emit()
+    
+    # function for fetching currently available ports
+    def get_ports(self):
+        return [port.portName() for port in QSerialPortInfo().availablePorts()]
+
 
 class SerialDynamicPlotter(QMainWindow):
     def __init__(self):
@@ -36,6 +62,8 @@ class SerialDynamicPlotter(QMainWindow):
         # initialize variables
         self.sensor_data = {}                                                                       # dictionary for storing the data
         self.com_port_names = []                                                                    # list for storing active COM ports
+        self.data_records = []                                                                      # list for storing sensor data for export
+        self.buffer_size = 7000
 
 
         # define GUI window dimentions characteristics
@@ -58,32 +86,56 @@ class SerialDynamicPlotter(QMainWindow):
         viewer_widget.setLayout(plot_widget_layout)
         self.setCentralWidget(viewer_widget)
 
-        # fetch currently available COM ports
-        self.available_ports = QSerialPortInfo().availablePorts()
-        self.com_port_names = [port.portName() for port in self.available_ports]
-
         # create drop down widgets for choosing COM port
         self.com_port_combo = QComboBox()
-        self.com_port_combo.addItems([str(com) for com in self.com_port_names])
-        self.com_port_combo.setCurrentIndex(0)
-        self.com_port_combo.currentIndexChanged.connect(self.change_com_port)
         plot_widget_layout.addWidget(self.com_port_combo)
 
-
+        # create instance of serial monitor
+        self.serial_monitor = SerialPortMonitor()
+        self.serial_monitor.port_changed.connect(self.update_com_port_combo)
+        self.update_com_port_combo()
+        
         # initialize and configure a QSerialPort object for serial communication
         self.serial_port = QSerialPort()
         self.serial_port.setPortName("COM11")
         self.serial_port.setBaudRate(9600)
-        #self.serial_port.readyRead.connect(self.receive_serial_data)
-        
+        self.serial_port.readyRead.connect(self.receive_data)
+
+    def update_com_port_combo(self):
+        current_port = self.com_port_combo.currentText()
+        self.com_port_combo.clear()
+        for port_name in self.serial_monitor.current_ports:
+            self.com_port_combo.addItem(port_name)
+        if current_port in self.serial_monitor.current_ports:
+            self.com_port_combo.setCurrentText(current_port)   
     
     def add_sensor(self, name, color):
-        pass
+        self.sensor_data[name] = {
+            'buffer': CircularBuffer(self.buffer_size),
+            'plot_data': self.plot_widget.plot(pen=pgt.mkPen(color, width=2), name=name),
+        }
 
     def change_com_port(self, index):
         #self.serial_port.setPortName(self.com_port_names[index])
         self.serial_port.setPortName(self.com_port_combo.currentText())
         #self.serial_port.readyRead.connect(self.receive_serial_data)
+    
+    # function to receive serial data
+    def receive_data(self):
+        while self.serial_port.canReadLine():
+            try:
+                data = self.serial_port.readLine().data().decode("utf-8").strip()
+                values = data.split(", ")
+                sensor_name = "P"
+                sensor_value = float(values[4].strip())
+                formatted_value = "{:.2f}".format(sensor_value)
+                if sensor_name in self.sensor_data:
+                    data_buffer = self.sensor_data[sensor_name]['buffer']
+                    data_buffer.push(formatted_value)
+                    self.sensor_data[sensor_name]['plot_data'].setData(data_buffer.get_data())
+                    self.data_records.append([sensor_name, sensor_value])
+            except(UnicodeDecodeError, IndexError, ValueError):
+                pass
 
 
 if __name__ == "__main__":
