82edf20c48c219d10997460faf6dbc52613b8067
tlasota <t.lasota@erba.com>
2024-06-14 10:40:42 +0100

    Add: add time of exp entry as well as notes field

--- database.py
+++ database.py
@@ -9,16 +9,19 @@ collection = db[credentials.COLLECTION_NAME]
 
 
 # function for saving data into databse
-def store_measurements(user_id, experiment_name, instrument_id, cartridge_number, measurements):
+def store_measurements(user_id, experiment_name, instrument_id, cartridge_number, measurements, notes):
     test_date = datetime.now().strftime("%Y-%m-%d")
+    test_time = datetime.now().strftime("%H:%M:%S")
 
     record = {
         "test_date": test_date,
+        "test_time": test_time,
         "user_id": user_id,
         "instrument_id": instrument_id,
         "experiment_name": experiment_name,
         "cartridge_number": cartridge_number,
         "measurements": measurements,
+        "notes": notes
     }
     result = collection.insert_one(record)
     confirmation_msg = f"Session data inserted with ID: {result.inserted_id}"
